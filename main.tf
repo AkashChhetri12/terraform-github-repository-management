f526aec33fb980d678e6ebdaf7634be83fb65870
Akash Chhetri <chhetriakash2616@gmail.com>
2024-09-30 17:20:49 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,40 @@
-resource "github_repository" "repo" {
-  name        = local.repo_config.repo_name
-  description = local.repo_config.repo_description
-  visibility  = local.repo_config.repo_visibility
-
-  has_issues        = local.repo_config.repo_features.has_issues
-  has_projects      = local.repo_config.repo_features.has_projects
-  has_wiki          = local.repo_config.repo_features.has_wiki
-  has_downloads     = local.repo_config.repo_features.has_downloads
-  has_discussions   = local.repo_config.repo_features.has_discussions
-
-  allow_merge_commit = local.repo_config.repo_settings.allow_merge_commit
-  allow_squash_merge = local.repo_config.repo_settings.allow_squash_merge
-  allow_rebase_merge = local.repo_config.repo_settings.allow_rebase_merge
-  allow_auto_merge   = local.repo_config.repo_settings.allow_auto_merge
-
-  delete_branch_on_merge = local.repo_config.repo_settings.delete_branch_on_merge
-  auto_init              = true
-
-  dynamic "template" {
-    for_each = local.repo_config.template_repo != null ? [1] : []
-    content {
-      owner      = local.repo_config.template_repo.owner
-      repository = local.repo_config.template_repo.name
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
     }
   }
 }
 
-resource "github_branch_protection" "main" {
-  repository_id = github_repository.repo.node_id
-  pattern       = "main"
+provider "github" {
+  token = var.github_token
+}
 
-  required_status_checks {
-    strict   = true
-    contexts = local.repo_config.required_status_checks
+locals {
+  repo_config = yamldecode(file("${path.module}/repo_config.yaml"))
+}
+
+resource "github_repository" "repo" {
+  name        = local.repo_config.name
+  description = local.repo_config.description
+  visibility  = local.repo_config.visibility
+
+  dynamic "features" {
+    for_each = local.repo_config.features
+    content {
+      name    = features.key
+      enabled = features.value
+    }
   }
 
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
-    required_approving_review_count = 1
+  dynamic "settings" {
+    for_each = local.repo_config.settings
+    content {
+      name  = settings.key
+      value = settings.value
+    }
   }
 
-  enforce_admins = true
+  topics = local.repo_config.topics
 }
