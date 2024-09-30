8b70f3adf4e03029ad5c3ee1217d486cad1be740
Akash Chhetri <chhetriakash2616@gmail.com>
2024-09-30 17:17:02 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,30 @@
-# Repository 
-resource "github_repository" "this" {
-  name        = var.repo_name
-  description = var.repo_description
-  visibility  = var.repo_visibility
-  has_issues        = var.repo_features.has_issues
-  has_projects      = var.repo_features.has_projects
-  has_wiki          = var.repo_features.has_wiki
-  has_downloads     = var.repo_features.has_downloads
-  has_discussions   = var.repo_features.has_discussions
-  allow_merge_commit = var.repo_settings.allow_merge_commit
-  allow_squash_merge = var.repo_settings.allow_squash_merge
-  allow_rebase_merge = var.repo_settings.allow_rebase_merge
-  allow_auto_merge   = var.repo_settings.allow_auto_merge
-  delete_branch_on_merge = var.repo_settings.delete_branch_on_merge
+resource "github_repository" "repo" {
+  name        = local.repo_config.repo_name
+  description = local.repo_config.repo_description
+  visibility  = local.repo_config.repo_visibility
+
+  has_issues        = local.repo_config.repo_features.has_issues
+  has_projects      = local.repo_config.repo_features.has_projects
+  has_wiki          = local.repo_config.repo_features.has_wiki
+  has_downloads     = local.repo_config.repo_features.has_downloads
+  has_discussions   = local.repo_config.repo_features.has_discussions
+
+  allow_merge_commit = local.repo_config.repo_settings.allow_merge_commit
+  allow_squash_merge = local.repo_config.repo_settings.allow_squash_merge
+  allow_rebase_merge = local.repo_config.repo_settings.allow_rebase_merge
+  allow_auto_merge   = local.repo_config.repo_settings.allow_auto_merge
+
+  delete_branch_on_merge = local.repo_config.repo_settings.delete_branch_on_merge
   auto_init              = true
 
   dynamic "template" {
-    for_each = var.template_repo != null ? [1] : []
+    for_each = local.repo_config.template_repo != null ? [1] : []
     content {
-      owner      = var.template_repo.owner
-      repository = var.template_repo.name
+      owner      = local.repo_config.template_repo.owner
+      repository = local.repo_config.template_repo.name
     }
   }
-  
-  topics = toset(var.is_terraform_module ? concat(["terraform"], var.topics) : var.topics)
 }
-
 
 resource "github_branch_protection" "main" {
   repository_id = github_repository.repo.node_id
@@ -33,7 +32,7 @@ resource "github_branch_protection" "main" {
 
   required_status_checks {
     strict   = true
-    contexts = var.required_status_checks
+    contexts = local.repo_config.required_status_checks
   }
 
   required_pull_request_reviews {
